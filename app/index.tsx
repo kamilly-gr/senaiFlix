4b9728b7bb5f68c3a0bddac1ec7b9936487a3cad
KAMILLY GOMES RIBEIRO <kamilly.gomes@portalsesisp.org.br>
2026-02-18 11:12:03 -0300

    add banner

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import { Dimensions, FlatList, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const EmAlta = [
@@ -70,18 +70,61 @@ const filmes = [
     image:"https://play-lh.googleusercontent.com/nz5s7-HJ3iijnvitzCwsa_oD5l7saKAnF_Hv3qGtsP6dtsXTqESZmfuQKqyZZD4UywyB"
   },
   
-
-
-
-
 ]
+
+const bannerData = [
+  {
+    id : "1",
+    image: 
+    "https://ingresso-a.akamaihd.net/b2b/production/uploads/article/image/3073/1cb40d9d526bb3a6c14f1916c3878a3b.jpg",
+    title:"As branquelas"
+  },
+  {
+    id : "2",
+    image: 
+    "https://s2-gshow.glbimg.com/GUr6WtMoAX8dHWOhk_HR4AgP3Xs=/0x0:1920x1008/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_e84042ef78cb4708aeebdf1c68c6cbd6/internal_photos/bs/2025/d/H/hgr1ovRKKbSNVRowpNLQ/fc0123e7dbc377ea51477af66e79fb72.jpg",
+    title:"Zootopia 2"
+  },
+  {
+    id : "3",
+    image: 
+    "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEimLO7Y9svjGR1KBPeiGZIoXJfOlrOj9wvoyC3_z1Q_5u-jvAnnVYDT2GC7miYnN3icGyQW4rCUfVGQZwRyF4C9GlOPeQV6XwF1lUCV9qtweGKahZdaqyW9QPfsAzE6JTxuoP2yoij5UoIA/s1600/00+-+Para+Todos+os+Garotos+Que+J%25C3%25A1+Amei+%2528To+All+the+Boys+I%25E2%2580%2599ve+Loved+Before%252C+2018%2529+-+Copia.jpg",
+    title:"Para todos os garotos que já amei"
+
+  }
+]
+
+const {width} = Dimensions.get("window")
 
 export default function Index() {
   return (
     <SafeAreaView style={style.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
       <View>
         <Text style={style.mainTitle}>KamisaFlix</Text>
         <Text style={style.welcomeText}>Bem vindo, ao seu catalogo de filmes e cinemas</Text>
+      </View>
+      <View style={style.section}>
+        <FlatList
+        data={bannerData}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={(({item}) => (
+          <View style={style.bannerContainer}>
+            <View style={style.bannerItem}>
+              <Image  style={style.bannerImage} source={{uri: item.image}}></Image>
+              <Text style={style.bannerTitle}>{item.title}</Text>
+              <TouchableOpacity style={style.bannerButton}>
+                <Text style={style.bannnerButtonText}>Assistir</Text>
+              </TouchableOpacity>
+
+            </View>
+          </View>
+
+
+        ))}
+        />
+
       </View>
 
         {/* Primeiro carrossel de filmes em alta */}
@@ -151,7 +194,7 @@ export default function Index() {
 
       </View>
 
-
+      </ScrollView>
 
     </SafeAreaView>
   );
@@ -203,6 +246,47 @@ const style = StyleSheet.create({
   },
   section:{
     marginBottom: 30
+  },
+  bannerImage:{
+    width:"100%",
+    height: 200,
+    resizeMode: "cover",
+    borderRadius: 8,
+    
+  },
+  bannerTitle:{
+    color:"#fff",
+    fontSize:20,
+    fontWeight: "bold",
+    position:"absolute",
+    bottom: 50,
+    left: 15
+  },
+  bannerItem:{
+    flex: 1,
+    height:200,
+    paddingHorizontal: 5,
+    position:"relative"
+  },
+
+  bannerContainer: {
+    width: width -30
+  },
+
+  bannerButton:{
+    backgroundColor: "#e50914",
+    borderRadius:5,
+    paddingVertical:6,
+    paddingHorizontal:16,
+    position:"absolute",
+    bottom: 10,
+    left:15
+
+  },
+  bannnerButtonText: {
+    color:"#fff",
+    fontSize:13,
+    fontWeight:"bold"
   }
 
 })
